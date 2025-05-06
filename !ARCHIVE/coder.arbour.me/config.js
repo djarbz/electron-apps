dce41c9a02839a3699fae93bcb1d3a0dba896a2c
default <austin@arbour.me>
2025-05-06 16:46:42 +0000

    remove ARCHIVE

--- !ARCHIVE/coder.arbour.me/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    WEB_URL: 'https://coder.arbour.me'
-};
-
-module.exports = config;
--- !ARCHIVE/web.app/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    WEB_URL: 'https://udm14.com/'
-};
-
-module.exports = config;
